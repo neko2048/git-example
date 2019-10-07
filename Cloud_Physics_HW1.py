1e638625746149f3377a40a0098b12ebfa925c12
neko2048 <38982885+neko2048@users.noreply.github.com>
2019-10-08 01:48:23 +0800

    correct the unit of g / kg to kg / kg

--- Cloud_Physics_HW1.py
+++ Cloud_Physics_HW1.py
@@ -17,9 +17,9 @@ def pressure_vapor(temp):
 	e_v = 611 * (e ** (L_v / R_v * (1 / T_0 - 1 / temp)))
 	return e_v # Pa
 def content_vapor(e_v, temp, pressure):
-	"""q_vs in g / kg"""
+	"""q_vs in kg / kg"""
 	q_vs = 0.622 * pressure_vapor(temp) / pressure /100
-	return q_vs #g / kg
+	return q_vs #kg / kg
 def potential_temp_e(temp, pressure, q_v):
 	"""theta_e, 
 	If the air parcel is saturared, use 9999 in q_v."""
@@ -41,7 +41,7 @@ pressure_a = 1000.
 t_d_a = 28 + 273. # dew_point temp.
 e_v_a = pressure_vapor(t_d_a) # use t_d_a since A is not saturated
 q_v_a = content_vapor(e_v_a, t_d_a, pressure_a)
-print("qv_a:", q_v_a, "g / kg")
+print("qv_a:", q_v_a, "kg / kg")
 theta_e_a = potential_temp_e(temp_a, pressure_a, q_v_a)
 print("theta_e in A: ", theta_e_a)
 # Condition B
@@ -50,12 +50,12 @@ pressure_b = 750.
 temp_b = 32 + 273. # Assumed temp.
 e_vs_b = pressure_vapor(temp_b)
 q_vs_b = content_vapor(e_vs_b, temp_b, pressure_b)
-print("qv_b:", q_vs_b, "g / kg")
+print("qv_b:", q_vs_b, "kg / kg")
 theta_e_b = potential_temp_e(temp_b, pressure_b, 9999) # 9999: Saturated
 print("theta_e in B: ", theta_e_b)
 #Iteration
 while True: #Stop if theta_e is same.
-	if (theta_e_b - theta_e_a) > 1 :
+	if (theta_e_b - theta_e_a) > 1e-3 :
 		temp_b -= 1e-5
 		theta_e_b = potential_temp_e(temp_b, pressure_b, 9999)
 		#print("theta_e_b: %f" % theta_e_b)
@@ -67,4 +67,4 @@ while True: #Stop if theta_e is same.
 e_vs_b = pressure_vapor(temp_b)
 print("e_vs_b:", e_vs_b/100, "hPa")
 q_vs_b = content_vapor(e_vs_b, temp_b, 750)
-print("q_vs_b:", q_vs_b, "g / kg")
+print("q_vs_b:", q_vs_b, "kg / kg")
